a85298b659dd644f75a72e3bc1bd6090ec4cbcd0
gaosj <saijie.gao@gamesvessel.com>
2022-01-08 17:30:14 +0800

    build

---
+++ homeservice/src/main/java/com/lanlanjia/llj_service/LljServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lanlanjia.llj_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LljServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LljServiceApplication.class, args);
+    }
+
+}
